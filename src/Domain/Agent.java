4a178d8ecaa43db75b2f365d007baa046e8d0438
samibarmil <77699305+samibarmil@users.noreply.github.com>
2021-01-20 11:54:30 -0600

    Revert "super Class with 3 child Classes"
    This reverts commit 74c27ab7b767c15cf522fd547c1acaaa248fe684.

--- src/Domain/Agent.java
+++ src/Domain/Agent.java
@@ -3,7 +3,6 @@ package Domain;
 
 
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,15 +14,5 @@ public class Agent extends Person {
 	// a minimum of one agent for 0 or many reservations
 
 	private List<Reservation> reservations = new ArrayList<Reservation>();
-	public Agent(String id ,String firstname,String lastName,
-			String email,LocalDate dob,Address address) {
-		super(id,firstname,lastName,email,dob ,address );
-	}
-	public List<Reservation>getReservations(){
-		return reservations;
-	}
-	public void addReservation(Reservation reservation) {
-		reservations.add(reservation);
-	}
-	
+
 }
