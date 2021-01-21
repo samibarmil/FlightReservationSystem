00e43e38e2c15682cb8afbc88b1643af26b46070
AhmadOsaili <ahmadosaili@hotmail.com>
2021-01-20 22:27:33 -0800

    Modifing the classes
    The unique random number functions were implemented where needed to meet the requirements

--- src/Domain/Main.java
+++ src/Domain/Main.java
@@ -1,21 +1,46 @@
 package Domain;
 
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.UUID;
 
 public class Main {
+//
+//	private static String randomAlphaNumeric() {
+//		String alphaNumericSet = "1234567890";
+//		StringBuilder randomValue = new StringBuilder();
+//		Random rnd = new Random();
+//		while (randomValue.length() < 20) { // length of the random string.
+//			int index = (int) (rnd.nextFloat() * 10);
+//			randomValue.append(alphaNumericSet.charAt(index));
+//		}
+//		return randomValue.toString();
+////
+////	}
+//	 static ArrayList<String> randomNumbersInUse = new ArrayList<>();
+//
+////	
+//	private static String randomAlphaNumeric(int alphaNumericLength) {
+//		String alphaNumericSet = "A";
+//		StringBuilder randomValue = new StringBuilder();
+//		Random rnd = new Random();
+//		while (randomValue.length() < alphaNumericLength) { // length of the random string.
+//			int index = (int) (rnd.nextFloat() * alphaNumericSet.length());
+//			randomValue.append(alphaNumericSet.charAt(index));
+//		}
+//
+//		while (true) {
+//			if (!randomNumbersInUse.contains(randomValue)) {
+//				randomNumbersInUse.add(randomValue.toString());
+//				break;
+//			} else {
+//				randomAlphaNumeric(alphaNumericLength);
+//			}
+//		}
+//		return randomValue.toString();
+//
+//	}
 
-	private static String randomAlphaNumeric() {
-		String alphaNumericSet = "1234567890";
-		StringBuilder randomValue = new StringBuilder();
-		Random rnd = new Random();
-		while (randomValue.length() < 20) { // length of the random string.
-			int index = (int) (rnd.nextFloat() * 10);
-			randomValue.append(alphaNumericSet.charAt(index));
-		}
-		return randomValue.toString();
-
-	}
 	public static void main(String args[]) {
 //
 //		try {
@@ -28,9 +53,13 @@ public class Main {
 //		catch (Exception e) {
 //			System.out.println(e.getMessage());
 //		}
-		System.out.println(randomAlphaNumeric());
+			Random r = new Random();
+			System.out.println(r.nextInt(3));
+			System.out.println(Ticket.randomAlphaNumeric());
+			System.out.println(Ticket.randomAlphaNumeric());
+			System.out.println(Ticket.randomAlphaNumeric());
 
-		// System.out.println(getSaltString());
+		
 	}
 
 }
--- src/Domain/Ticket.java
+++ src/Domain/Ticket.java
@@ -1,5 +1,6 @@
 package Domain;
 
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.UUID;
 
@@ -8,11 +9,34 @@ public class Ticket {
 	private final String id;
 	private final String ticketNumber; // 20 digits random
 	private FlightInstance specificFlight;
+	// To make sure that the generated random number wasn't used before
+	public static ArrayList<String> randomNumbersInUse = new ArrayList<>();
 
 	public Ticket(FlightInstance flight) {
 		this.id = UUID.randomUUID().toString();
 		this.ticketNumber = UUID.randomUUID().toString();
 		this.specificFlight = flight;
+	}
+
+	// A unique 20 digits random will be generated here
+	public static String randomNumeric() {
+		String numeralsSet = "1234567890";
+		StringBuilder randomValue = new StringBuilder();
+		Random rnd = new Random();
+		while (randomValue.length() < 20) { // length of the random string
+			int index = (int) (rnd.nextFloat() * numeralsSet.length());
+			randomValue.append(numeralsSet.charAt(index));
+		}
+		while (true) {
+			if (!randomNumbersInUse.contains(randomValue.toString())) {
+				randomNumbersInUse.add(randomValue.toString());
+				break;
+			}
+			randomNumeric();
+		}
+
+		return randomValue.toString();
+
 	}
 
 	public String getId() {
